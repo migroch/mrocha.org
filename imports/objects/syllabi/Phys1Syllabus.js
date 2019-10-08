fbff570fbfcf36f53099ed8f8c1e3a5fd5d99cd3
Miguel Rocha <miguel@scitecha.com>
2019-10-08 09:59:48 -0700

    Class updates

--- imports/objects/syllabi/Phys1Syllabus.js
+++ imports/objects/syllabi/Phys1Syllabus.js
@@ -33,7 +33,7 @@ export default  Phys1Syllabus ={
     CourseSchedule:{
 	"Week 1: Intro to Physics and Scientific Measurements":["Chapters: 1", "<a href='http://slides.com/migroch/deck-2-4/fullscreen' target='_blank'>What is Physics, What is Science and Scientific Measurements</a>", "<a href='Phys1-Lab1.pdf' target='_blank'>Lab 1: Measuring the Diameter of the Sun</a>"],
 
-	"Week 2: Inertia and Newton's Laws of Motion":["Chapters: 2, 3, 4, 5", "<a  href='http://slides.com/migroch/deck-1-3-5/fullscreen' target='_blank'>Newton's Laws of Motion</a>","Assignments due Friday: <a href='Phys1-Lab1.pdf' target='_blank'>Lab 1: Measuring the diameter of the Sun</a> and <a href='Phys1-Hw1.pdf' target='_blank'>HW 1: Unit Conversion Practice</a>"],
+	"Week 2: Inertia and Newton's Laws of Motion":["Chapters: 2, 3, 4, 5", "<a  href='http://slides.com/migroch/deck-1-3-5/fullscreen' target='_blank'>Newton's Laws of Motion</a>","Assignments due Friday: <a href='Phys1-Lab1.pdf' target='_blank'>Lab 1: Measuring the diameter of the Sun</a> and <a href='Phys1-Hw1.pdf' target='_blank'>HW 1: Unit Conversion Practice</a> (<a href='HW1-Solutions.pdf' target='_blank'>HW 1 Solutions</a>)"],
 
 	"Week 3: Momentum and Energy":["Chapters: 6, 7", "<a href='http://slides.com/migroch/deck-1-3-7/fullscreen' target='_blank'>Momentum and Energy</a>", "Assignments due Friday: <a href='https://ilearn.gavilan.edu/courses/7263/quizzes/18271' target='_blank'>HW 2</a>, <a  href='Phys1-Lab2.pdf' target='_blank'>Lab 2: Equilibrium on an Inclined Plane</a> and <a  href='Phys1-Lab3.pdf' target='_blank'>Lab 3: Constant Acceleration</a>"],
 
