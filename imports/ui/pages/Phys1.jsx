d547a5a8ad60eb18acd2ef34482beccda9ce35cb
Miguel Rocha <miguel@scitecha.com>
2019-12-05 12:02:41 -0800

    Updates

--- imports/ui/pages/Phys1.jsx
+++ imports/ui/pages/Phys1.jsx
@@ -22,8 +22,8 @@ export default class Phys1 extends React.Component {
 
 	 <p dangerouslySetInnerHTML={{__html: weekEvents[1]}}></p>
 	 
-	 <p><a  href='Phys1-Lab19.pdf' target='_blank'>Lab 19: Emission Spectra</a></p>
-
+	 <p><a  href='https://slides.com/migroch/deck-2-4-9-12-36/fullscreen' target='_blank'>Midterm 3 Study Guide</a>, <a href="https://ilearn.gavilan.edu/courses/7263/quizzes/18281?module_item_id=214656">Midterm 3 Practice Quiz</a></p>
+	 
 	 <p className="bg-warning" dangerouslySetInnerHTML={{__html: weekEvents[2]}}></p>
 
 	 {/* <p className="bg-danger">Midterm 2 next Tuesday, October 29th - <a  href='https://slides.com/migroch/deck-1-3-7-11-34/fullscreen#/' target='_blank'>Midterm 2 Study Guide</a> - <a href='https://ilearn.gavilan.edu/courses/7263/quizzes/18290?module_item_id=214637'>HW 6 (Midterm 2 Practice Quiz)</a></p> */}
@@ -91,7 +91,7 @@ export default class Phys1 extends React.Component {
 	    <div className="container-fluid col-md-6 calendar">
 	      <iframe  src="https://calendar.google.com/calendar/embed?showTitle=0&amp;mode=AGENDA&amp;height=250&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=ko0gmi48tb2rbh591cuu8qnb80%40group.calendar.google.com&amp;color=%230F4B38&amp;ctz=America%2FLos_Angeles" style={{borderWidth:0}} width="100%" height="250" frameBorder="0" scrolling="no" fontSize="500"></iframe>
 	    </div>
-	    
+	   
 	  </div>
 
 	  <div className="container">
